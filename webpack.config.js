aadfed41e9fbcc121fbf39df4791b6319e38821d
WMXPY <wm@mengw.io>
2017-10-05 10:45:18 -0500

    add loader dependence and  to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
             test: /\.jsx?$/,
             use: ['babel-loader'],
             exclude: /node_modules/
+        }, {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
     },
     plugins: [

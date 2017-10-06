8b109f5ee7e156eae7c699e3e9a53fe81e50fbd0
Meng Wang <wm@mengw.io>
2017-10-06 14:28:11 -0500

    Change the router to hashrouter that fix when in production enviorment, use .html file to display

--- src/renderer/app.jsx
+++ src/renderer/app.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import ROne from './RouteOne';
 import RTwo from './RouteTwo';
 
@@ -7,12 +7,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <HashRouter>
           <div>
             <Route exact={true} path="/" component={ROne} />
             <Route path="/t" component={RTwo} />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </div>
     );
   }
